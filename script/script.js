cf437120d9c20d4db6b50bf278c226a89660aaa5
Daniel Zhang <D.zhang9707@gmail.com>
2023-04-15 13:21:36 +1200

    added tech blog sprint 3

--- script/script.js
+++ script/script.js
@@ -1,7 +1,18 @@
 function turnDark() {
-  document.getElementsByClassName('information').style.background-color = 'black';
-  document.getElementsByClassName('information').style.color = 'white';
-  document.getElementsByClassName('information').innerHTML = 'checkcehck'
+  const turner = document.querySelector('.information')
+  turner.classList.toggle('darkmode')
+  turner.innerHTML = 'You pressed it!'
+  console.log('this is working')
+
+  const visible = document.querySelectorAll('.invisi')
+  for (let i = 0; i < visible.length; i++) {
+    visible[i].classList.toggle('darkmode')
+  }
+
+  const headers = document.querySelectorAll('.toggler')
+  for (let i = 0; i < headers.length; i++) {
+    headers[i].classList.toggle('cantSee')
+  }
 }
 
 document.getElementById('darkMode').onclick = turnDark
